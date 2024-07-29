ad684f150b420fe423ad239f2ddd1d7367a5fcaa
Sachin Vaish <vaish139@gmail.com>
2024-07-29 09:11:31 +0530

    React Player Setup for My Camera Stream

--- client/src/components/ChatRoom.jsx
+++ client/src/components/ChatRoom.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { createRoutesFromElements, useNavigate, useParams } from 'react-router-dom'
 import { useSocket } from '../context/SocketProvider';
+import ReactPlayer from 'react-player';
+import { FaVideo, FaVideoSlash } from "react-icons/fa6";
 
 export default function ChatRoom() {
   const {roomID} = useParams();
   const navigate = useNavigate();
   const socket = useSocket();
-  const [remoteSocketID, setRemoteSocketID]=useState('');
-  const [remoteEmail,setRemoteEmail]=useState('')
+  const [remoteUser, setRemoteUser]=useState({});
   const [members, setMembers] = useState([]);
+  const [myCamera,setMyCamera]=useState(false);
+  const [myStream, setMyStream]=useState('');
 
   const leaveRoom = ()=>{
     socket.emit('room:exit',socket.id);
@@ -16,15 +19,38 @@ export default function ChatRoom() {
   }
 
   const handleUserJoined = (data)=>{
-    const {email, socketID} = data;
-    console.log(`${email} joined the Chatroom`);
-    setRemoteSocketID(socketID);
-    setRemoteEmail(email);
+    const {email, id} = data;
+    console.log(`${email} joined the Chatroom  ID : ${id}`);
+    setRemoteUser({email,id})
     setMembers((members)=>[...members,email]);
   }
 
   const handleUserLeft = (data)=>{
       console.log(`${data} Left the Room`);
+      setRemoteSocket('');
+      setRemoteEmail('');
+  }
+
+  const handleCall=()=>{
+    console.log('Calling the Remote User');
+    // const stream = navigator.mediaDevices.getUserMedia({video: true, audio:true});
+    // if(stream){
+    //   setMyStream(stream);
+    // }
+  }
+
+  const handleStream=async()=>{
+    let stream;
+    if(!myCamera){
+      stream = await navigator.mediaDevices.getUserMedia({video: true, audio:true});
+      if(stream){
+        setMyStream(stream);
+        setMyCamera(true)
+      }
+    }else{
+      setMyStream('');
+      setMyCamera(false)
+    }
   }
 
   useEffect(()=>{
@@ -43,20 +69,28 @@ export default function ChatRoom() {
         <h1 className="sm:text-5xl text-3xl text-blue-800 font-bold mb-10 ">Chat Room</h1>
         <button className='btn btn-danger ' onClick={leaveRoom}>Leave Room</button>
       </div>
-      <div id='videoStream' className='flex flex-col lg:flex-row gap-5  min-w-max items-center lg:justify-center mt-5'>
+      {remoteUser.id ? <h2>You're joined in Room : {roomID}</h2> : <h2>No one in this Room</h2>}
+      {remoteUser.id &&
+        <div id='videoStream' className='flex flex-col lg:flex-row gap-5  min-w-max items-center lg:justify-center mt-5'>
         <div id='user-stream' className='flex flex-col'>
           <h3 className=' font-bold'>You</h3>
-          <div style={{width:'30em', height:'20em'}} className=' bg-slate-400'>Your Stream</div>
+          {/* <div style={{width:'30em', height:'20em'}} className=' bg-slate-400'>Your Stream</div> */}
+          <ReactPlayer playing url={myStream} width={'30em'} height={'20em'} style={{border:'2px solid red'}} />
+          <div className='flex gap-4'>
+            {myCamera ? <FaVideoSlash onClick={handleStream}/> : <FaVideo onClick={handleStream}/>}
+          </div>
         </div>
-        {remoteEmail &&
+        {remoteUser.email &&
           <div id='remote-user-stream' className='flex flex-col'>
-          <h3 className=' font-bold'>{remoteEmail}</h3>
+          <h3 className=' font-bold'>{remoteUser.email}</h3>
           <div style={{width:'30em', height:'20em'}} className=' bg-slate-400'>Receiver's Stream</div>
         </div>
         }
       </div>
+
+      }
       <div id='controls' className='flex justify-center w-full'>
-          <button className='btn btn-green'>Accept Call</button>
+          {remoteUser.id && <button className='btn btn-green' onClick={handleCall}>Call</button>}
         </div>
     </div>
   )
