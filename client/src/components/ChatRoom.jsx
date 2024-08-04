82b6a5730ec18a30dbf18bb6303c178f7720d517
Sachin Vaish <vaish139@gmail.com>
2024-08-04 23:29:30 +0530

    Issue : getTrack methond getting empty stream

--- client/src/components/ChatRoom.jsx
+++ client/src/components/ChatRoom.jsx
@@ -3,17 +3,16 @@ import { createRoutesFromElements, useNavigate, useParams } from 'react-router-d
 import { useSocket } from '../context/SocketProvider';
 import ReactPlayer from 'react-player';
 import { FaVideo, FaVideoSlash } from "react-icons/fa6";
+import peer from '../service/peer';
 
 export default function ChatRoom() {
   const {roomID} = useParams();
   const navigate = useNavigate();
   const socket = useSocket();
   const [remoteUser, setRemoteUser]=useState({});
-  const [members, setMembers] = useState([]);
   const [myCamera,setMyCamera]=useState(false);
   const [myStream, setMyStream]=useState('');
-
-  console.log( members);
+  const [remoteStream, setRemoteStream]=useState('');
 
   const leaveRoom = ()=>{
     socket.emit('room:exit',socket.id);
@@ -24,43 +23,86 @@ export default function ChatRoom() {
     const {email, id} = data;
     console.log(`${email} joined the Chatroom  ID : ${id}`);
     setRemoteUser({email,id})
-    setMembers((members)=>[...members,email]);
   }
 
   const handleUserLeft = (data)=>{
       console.log(`${data} Left the Room`);
-      setRemoteSocket();
-      setRemoteEmail();
+      setRemoteUser('');
   }
 
-  const handleCall=()=>{
+  const handleCall=async()=>{
     console.log('Calling the Remote User');
-    // const stream = navigator.mediaDevices.getUserMedia({video: true, audio:true});
-    // if(stream){
-    //   setMyStream(stream);
-    // }
+    const stream = await navigator.mediaDevices.getUserMedia({video: true, audio:true});
+    const offer = await peer.getOffer();
+    socket.emit('user:call',{ to:remoteUser.id, offer})
+    setMyStream(stream);
   }
 
-  const handleStream=async()=>{
-    let stream;
-    if(!myCamera){
-      stream = await navigator.mediaDevices.getUserMedia({video: true, audio:true});
-      if(stream){
-        setMyStream(stream);
-        setMyCamera(true)
-      }
-    }else{
-      setMyStream('');
-      setMyCamera(false)
+  const handleIncomingCall=async({from, offer})=>{
+    // await handleStream();
+    setRemoteUser({...remoteUser, id:from});
+    const stream = await navigator.mediaDevices.getUserMedia({video: true, audio:true});
+    setMyStream(stream);
+    console.log('incoming call from :',from, offer)
+    const ans = await peer.getAnswer(offer);
+    socket.emit('call:accepted',{to:from, ans});
+  }
+
+  const sendStreams = ()=>{
+    for(const track of myStream.getTracks()){
+      peer.peer.addTrack(track, myStream);
     }
   }
+
+  const handleCallAccepted = async({from, ans})=>{
+    setRemoteUser({...remoteUser, id:from});
+    peer.setLocalDescription(ans);
+    // await handleStream();
+    console.log({myStream, myCamera},remoteUser.email);
+    console.log('call accepted');
+    sendStreams();
+  }
+
+  // const handleStream=async()=>{
+  //   let stream;
+  //   // if(!myCamera){
+  //     console.log(`mycamera is ${myCamera} state`)
+      
+  //     if(stream){
+  //       setMyStream(stream);
+  //       console.log('setting myCamera == true')
+  //       setMyCamera(true);
+  //       console.log('in HandleStream()',{stream, myStream, myCamera});
+  //     }
+      
+  //   // }else{
+  //   //   console.log('mycamera is ON state')
+  //   //   setMyStream('');
+  //   //   setMyCamera(false)
+  //   // }
+  // }
+
+
+  useEffect(()=>{
+    peer.peer.addEventListener('track',async(ev)=>{
+      const remoteStream = ev.streams
+      setRemoteStream(remoteStream[0]);
+      console.log('remote stream',remoteStream)
+    })
+    console.log('useEffect track-eventlistener attached',{myCamera});
+  },[])
 
   useEffect(()=>{
       socket.on('user:joined',handleUserJoined)
       socket.on('user:left',handleUserLeft)
+      socket.on('incoming:call', handleIncomingCall);
+      socket.on('call:accepted', handleCallAccepted);
+      console.log('common useEffect called',{myCamera});
       return ()=>{
         socket.off('user:joined',handleUserJoined);
         socket.off('user:left',handleUserLeft);
+        socket.off('incoming:call', handleIncomingCall);
+        socket.off('call:accepted', handleCallAccepted);
       }
   },[socket])
 
@@ -78,14 +120,12 @@ export default function ChatRoom() {
           <h3 className=' font-bold'>You</h3>
           {/* <div style={{width:'30em', height:'20em'}} className=' bg-slate-400'>Your Stream</div> */}
           <ReactPlayer playing url={myStream} width={'30em'} height={'20em'} style={{border:'2px solid red',transform:'rotateY(180deg)'}} />
-          <div className='flex gap-4'>
-            {myCamera ? <FaVideoSlash onClick={handleStream}/> : <FaVideo onClick={handleStream}/>}
-          </div>
         </div>
         {remoteUser.email &&
           <div id='remote-user-stream' className='flex flex-col'>
           <h3 className=' font-bold'>{remoteUser.email}</h3>
-          <div style={{width:'30em', height:'20em'}} className=' bg-slate-400'>Receiver's Stream</div>
+          {/* <div style={{width:'30em', height:'20em'}} className=' bg-slate-400'>Receiver's Stream</div> */}
+          <ReactPlayer playing url={remoteStream} width={'30em'} height={'20em'} style={{border:'2px solid red'}} />
         </div>
         }
       </div>
--- server/index.js
+++ server/index.js
@@ -17,11 +17,23 @@ io.on('connection',(socket)=>{
 
         io.to(roomID).emit('user:joined',{email, id:socket.id});
         socket.join(roomID);
+
         io.to(socket.id).emit('room:join',(data));
+
         socket.on('room:exit',(data)=>{
             io.to(roomID).emit('user:left',socketToEmail.get(data));
         })
     })
+
+    socket.on('user:call',({to, offer})=>{
+        io.to(to).emit('incoming:call',{from:socket.id, offer});
+    })
+
+    socket.on('call:accepted',({to,ans})=>{
+        io.to(to).emit('call:accepted',{from:socket.id, ans})
+    })
+
+    
 })
 
 
